d82790c8e34d9d95ea4047d6a2e3e0d2d5e6564b
Phil-ATester <philatqa@gmail.com>
2021-04-12 17:30:47 +0100

    added extent reporting

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,13 +1,22 @@
 package tests;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import pages.AccountPage;
 import pages.Homepage;
 import pages.LoginPage;
+import utils.ExtentReports.ExtentTestManager;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -22,6 +31,15 @@ public class LoginTests {
     private String password = "password89";
     private String wrongPassword = "wrongpassword";
 
+    static ExtentTest test;
+    static ExtentReports report;
+
+    @BeforeAll
+    public static void startTest(){
+        report = new ExtentReports(System.getProperty("user.dir") + "/ExtentReports/ExtentReportResults.html");
+        test = report.startTest("Login Test");
+    }
+
     @BeforeEach
     public void setup(){
         WebDriverManager.chromedriver().setup();
@@ -34,16 +52,25 @@ public class LoginTests {
     @Test
     @DisplayName("SuccessFully Login Test 001")
     @Order(1)
-    public void ValidUserLoginTest(){
+    public void ValidUserLoginTest() throws IOException {
+
         //System.out.println("This is the test code " + new Object(){}.getClass().getEnclosingMethod().getName());
         Homepage homepage = new Homepage(driver);
         LoginPage loginPage = homepage.clickLoginLink();
         loginPage.setEmailAddress(email);
-        loginPage.setPassword(password);
+        loginPage.setPassword("password2");
         AccountPage accountPage = loginPage.clickLoginButton();
         String actualTitle = accountPage.getPageTitle();
         assertNotNull(actualTitle);
         assertEquals("My Account", actualTitle);
+        if (driver.getTitle().equals("My Account")){
+            test.log(LogStatus.PASS, "Navigated to Account Page");
+        }else{
+            //test.log(LogStatus.FAIL, "Tested Failed");
+            test.log(LogStatus.FAIL, test.addScreenCapture(capture(driver)) + "Test Failed");
+        }
+
+
         //System.out.println("Test End " + new Object(){}.getClass().getEnclosingMethod().getName());
     }
 
@@ -62,6 +89,7 @@ public class LoginTests {
         String alertMessage = loginPage.alertMessage();
         assertNotNull(alertMessage);
         assertEquals("Warning: No match for E-Mail Address and/or Password.", alertMessage);
+
     }
 
     @Test
@@ -86,5 +114,17 @@ public class LoginTests {
         if (driver != null) {
             driver.quit();
         }
+
+        report.endTest(test);
+        report.flush();
+    }
+
+    public static String capture(WebDriver driver) throws IOException {
+        String workingDir = System.getProperty("user.dir");
+        File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        File destination = new File(workingDir + "/ScreenShots/" + System.currentTimeMillis() + ".png");
+        String errorFilePath = destination.getAbsolutePath();
+        FileUtils.copyFile(srcFile, destination);
+        return errorFilePath;
     }
 }
